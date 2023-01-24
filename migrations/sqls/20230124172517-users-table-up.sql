9c072a60e1d3b3e5875961a90803ec0fdd336bcd
mazenaldaher <mazen0aldaher@gmail.com>
2023-01-24 19:45:55 +0200

    configure migration

---
+++ migrations/sqls/20230124172517-users-table-up.sql
@@ -0,0 +1,10 @@
+/* Replace with your SQL commands */
+--create table users
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(50) UNIQUE,
+    user_name VARCHAR(50) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL 
+);
